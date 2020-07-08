121c3ae308ccfb9c4052a371965b155d162635f9
Steve <smiths917@gmail.com>
2020-07-08 17:27:57 -0500

    Basic UI
    -Added flexible grid layout for empty tiles
    -Added default assets for tile pieces

---
+++ Assets/Scripts/FlexibleGridLayout.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleGridLayout : LayoutGroup
+{
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        float sqrRt = Mathf.Sqrt(transform.childCount);
+        rows = Mathf.CeilToInt(sqrRt);
+        columns = Mathf.CeilToInt(sqrRt);
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - spacing.x - (padding.left / (float)columns) - (padding.right / (float)columns);
+        float cellHeight = (parentHeight / (float)rows) - spacing.y - (padding.top / (float)rows) - (padding.bottom / (float)rows);
+
+        cellSize.x = cellWidth;
+        cellSize.y = cellHeight;
+
+        int columnCount = 0;
+        int rowCount = 0;
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / columns;
+            columnCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xSpacing = (spacing.x / 2 * (columnCount + 1)) + (spacing.x / 2 * columnCount);
+            var ySpacing = (spacing.y / 2 * (rowCount + 1)) + (spacing.y / 2 * rowCount);
+
+            var xPos = (cellSize.x * columnCount) + xSpacing + padding.left;
+            var yPos = (cellSize.y * rowCount) + ySpacing + padding.top;
+            
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+    public override void CalculateLayoutInputVertical()
+    {
+
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+
+    }
+
+    public override void SetLayoutVertical()
+    {
+
+    }
+}
